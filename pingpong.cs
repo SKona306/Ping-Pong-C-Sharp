2ae4b472d863ab7f910976310e43b851434210e7
Shaun Kent <shaunkent81@gmail.com>
2021-08-29 15:36:50 -0700

    add initial pingpong.cs file for backend

---
+++ pingpong.cs
@@ -0,0 +1,17 @@
+using System;
+
+class PingPong
+{
+  static void Main()
+  {
+    Console.WriteLine("Give a number and play a game: ");
+    string stringNumber = Console.ReadLine();
+    int number = int.Parse(stringNumber);
+
+    Console.WriteLine("The Game is On!");
+    for(int i = 0; i <= number; i++)
+    {
+      
+    }
+  }
+}
